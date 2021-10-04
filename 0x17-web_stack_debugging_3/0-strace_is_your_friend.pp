8daae45138485b8f8c390edb7e8e8fb328d32999
Eddie Hg <47726314+01Eddie@users.noreply.github.com>
2021-10-03 22:18:08 -0500

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # 0. Strace is your friend
-
 exec { 'wordpress':
-  command  => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path     => '/bin',
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => '/bin',
 }
