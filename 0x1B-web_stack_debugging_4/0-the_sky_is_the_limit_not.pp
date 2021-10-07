6a4612d88a66f0b2324b4ce20f7a50cdcb763dec
Eddie Hg <47726314+01Eddie@users.noreply.github.com>
2021-10-07 01:32:36 -0500

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,8 @@
 # Sky is the limit, let's bring that limit higher
 exec { 'limit_req':
   command => 'sed -i "s/15/3000/" /etc/default/nginx',
-  path    => '/bin'
+  path    => '/bin',
 }
 exec { 'nginx_restart':
-  command => '/usr/sbin/service nginx restart'
+  command => '/usr/sbin/service nginx restart',
 }
